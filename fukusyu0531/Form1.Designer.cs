2c199a417ebefee4ffbf6f6db5b156b3f65b05ab
satoshigp <satoshi3104>
2019-05-31 13:30:08 +0900

    なんか増えた

--- fukusyu0531/Form1.Designer.cs
+++ fukusyu0531/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -48,11 +51,42 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(262, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(226, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(541, 376);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -66,6 +100,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- fukusyu0531/Form1.cs
+++ fukusyu0531/Form1.cs
@@ -14,6 +14,8 @@ namespace fukusyu0531
     {
         int vx = -10;
         int vy = -10;
+        int x2 = -10, x3 = 5, x4 = -7;
+        int y2 = 10, y3 = -5, y4 = -7;
 
         public Form1()
         {
@@ -38,9 +40,50 @@ namespace fukusyu0531
                 vy = -vy;
             if (label1.Top < 0)
                 vy = -vy;
+
+            label2.Left += x2;
+            label2.Top += y2;
+
+            if (label2.Left > this.Width - 50)
+                x2 = -x2;
+            if (label2.Left < 0)
+                x2 = -x2;
+            if (label2.Top > this.Height - 50)
+                y2 = -y2;
+            if (label2.Top < 0)
+                y2 = -y2;
+
+            label3.Left += x3;
+            label3.Top += y3;
+
+            if (label3.Left > this.Width - 50)
+                x3 = -x3;
+            if (label3.Left < 0)
+                x3 = -x3;
+            if (label3.Top > this.Height - 50)
+                y3 = -y3;
+            if (label3.Top < 0)
+                y3 = -y3;
+
+            label4.Left += x4;
+            label4.Top += y4;
+
+            if (label4.Left > this.Width - 50)
+                x4 = -x4;
+            if (label4.Left < 0)
+                x4 = -x4;
+            if (label4.Top > this.Height - 50)
+                y4 = -y4;
+            if (label4.Top < 0)
+                y4 = -y4;
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
